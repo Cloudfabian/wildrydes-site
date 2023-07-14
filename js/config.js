ab48fb65d05ffeca0aec8fdae6e35348ca5afeec
Fabian Ukwa <user@MACs-MacBook-Pro.local>
2023-07-15 00:44:42 +0100

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_xgwf7yKg0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7f3u3pq2rv6jv2946ovssj04ld', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
